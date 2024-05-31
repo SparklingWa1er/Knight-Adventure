9611907c94958503f80075b3135bef1912b4f719
Nguyễn Minh Triết <144928521+SparklingWa1er@users.noreply.github.com>
2024-06-01 02:10:14 +0700

    Update main.py

--- main.py
+++ main.py
@@ -7,13 +7,16 @@ class Game:
         self.screen=pygame.display.set_mode((screen_width,screen_height))
         self.clock=pygame.time.Clock()
         self.level=Level()
-        pygame.display.set_caption("Chém quái")
+        pygame.display.set_caption("Knight Adventure")
     def run(self):
         while(True):
             for event in pygame.event.get():
                 if event.type==pygame.QUIT:
                     pygame.quit()
                     sys.exit()
+                if event.type==pygame.KEYDOWN:
+                    if event.key==pygame.K_m:
+                        self.level.toggle_menu()
             self.screen.fill((102, 217, 255))
             self.level.run()
             pygame.display.update()
