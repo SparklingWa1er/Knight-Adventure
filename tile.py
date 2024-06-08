e2eb8df60c9a1546ff679853685d923386e5bdc9
Nguyễn Minh Triết <144928521+SparklingWa1er@users.noreply.github.com>
2024-06-08 09:29:06 +0700

    Delete tile.py

--- tile.py
+++
@@ -1,9 +0,0 @@
-import pygame
-from config import *
-class Tile(pygame.sprite.Sprite):
-    def __init__(self,pos,groups,sprite_type,surface=pygame.Surface((tile_size,tile_size))):
-        super().__init__(groups)
-        self.image=surface
-        self.rect=self.image.get_rect(topleft=pos)
-        self.hitbox=self.rect.inflate(0,-hitbox_offset_y[sprite_type])
-        self.type=sprite_type
