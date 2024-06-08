b3fc644f0adc4e6314b9112dbd97b5e4f5aed0ee
Nguyễn Minh Triết <144928521+SparklingWa1er@users.noreply.github.com>
2024-06-08 09:27:41 +0700

    Delete doccsv.py

--- doccsv.py
+++
@@ -1,8 +0,0 @@
-from csv import reader
-def doccsv(path):
-	terrain_map = []
-	with open(path) as level_map:
-		layout = reader(level_map,delimiter = ',')
-		for row in layout:
-			terrain_map.append(list(row))
-		return terrain_map
